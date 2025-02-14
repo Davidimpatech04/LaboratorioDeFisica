637dbe1489b2ea573dab82a72362035381de5196
Davidimpatech04 <al.david.oliveira@impatech.org.br>
2025-02-14 20:02:55 -0300

    fodasi

--- projeto/data.py
+++ projeto/data.py
@@ -1,16 +1,32 @@
-{
- "cells": [
-  {
-   "cell_type": "markdown",
-   "metadata": {},
-   "source": []
-  }
- ],
- "metadata": {
-  "language_info": {
-   "name": "python"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+import serial
+import time
+
+with open("lll.csv","w") as f:
+    f.write("temperatura atual, tempo")
+    f.write("\n")
+    porta = "COM5"
+    baudrate = 115200
+
+    arduino = serial.Serial(porta, baudrate, timeout=1)
+
+    if arduino.is_open:
+        print("Porta aberta com sucesso!")
+
+    try:
+        while True:
+            if arduino.in_waiting > 0:
+                dados = arduino.read(arduino.in_waiting)
+                print(f'Dados recebidos: {dados.decode()}')
+                a = (dados.decode()).split()
+                f.write(f"{float(a[0])}, {float(a[1])}, {float(a[2])}") 
+                f.write("\n")
+            else:
+                print("Aguardando dados...")  
+            
+            time.sleep(0.1)  
+
+    except KeyboardInterrupt:
+        print("Leitura interrompida pelo usuário.")
+
+    finally:
+        arduino.close()
